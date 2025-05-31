e6a36c76e5efafd4591c9ff3b0f6dafd8001ca97
foysal777 <foysal.cse11@gmail.com>
2025-05-31 10:39:01 +0600

    Jwt Authentication completed

--- jwt_Authentication/settings.py
+++ jwt_Authentication/settings.py
@@ -30,14 +30,51 @@ INSTALLED_APPS = [
     'jwt_auth',
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
 
+REST_FRAMEWORK = {
+    "DATETIME_FORMAT": "%Y-%m-%d %H:%M:%S",
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+       
+    ],   
+} 
+
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -99,10 +136,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend', 
-    'jwt_auth.backend.EmailAuthBackend',          
-]
 
 # Email verification 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -112,6 +145,10 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER ='foysal.cse11@gmail.com'
 EMAIL_HOST_PASSWORD ='swxnxvdtcshucviv'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+
+
 
 
 # Internationalization
--- jwt_auth/urls.py
+++ jwt_auth/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path
-# from .views import (
-#     UserAPIView, RegisterAPIView, LoginAPIView, LogoutAPIView, VerifyOTPApiView, ResendOTPApiView
-# )
-# from rest_framework_simplejwt.views import (
-#     TokenRefreshView,
-# )
+from .views import (
+    UserAPIView, 
+    RegisterAPIView, VerifyOTPApiView,ResendOTPApiView,
+     LoginAPIView,
+     LogoutAPIView, 
+)
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 urlpatterns = [
    
+    path('user_all/', UserAPIView.as_view(), name='user-list'),  
+    path('register/', RegisterAPIView.as_view(), name='register'),
+    path('verify_otp/', VerifyOTPApiView.as_view(), name='verify-otp'),
+    path('resend_otp/', ResendOTPApiView.as_view(), name='resend-otp'),
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
